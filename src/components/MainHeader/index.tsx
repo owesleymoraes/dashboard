be0eb4e22210746881aa6b2a36d01aea92d21d09
Wesley Moraes <wesleyjava88@gmail.com>
2023-01-29 21:23:30 -0300

    feat: create component MainHeader

---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as Styled from './styles'
+
+export const MainHeader: React.FC = () => {
+    return (<Styled.Container>
+        MainHeader
+    </Styled.Container>)
+}
