f1476e476510a28b5ed06832afbf1bdd406c7d02
Wesley Moraes <wesleyjava88@gmail.com>
2023-01-29 21:22:37 -0300

    feat: create component aside

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as Styled from './styles'
+
+export const Aside: React.FC = () => {
+    return (
+        <Styled.Container>
+            Aside
+        </Styled.Container>
+    )
+
+}
