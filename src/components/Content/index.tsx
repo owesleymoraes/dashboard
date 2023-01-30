f58ca5bb43a6bb675f419f3f91fa43a8faa01820
Wesley Moraes <wesleyjava88@gmail.com>
2023-01-29 21:23:03 -0300

    feat: create component Content

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as Styled from './styles'
+
+export const Content: React.FC = () => {
+    return (
+        <Styled.Container>
+            Content
+        </Styled.Container>
+    )
+
+}
