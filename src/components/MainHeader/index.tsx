df37dc0c3cedcf8c9663e88e619b24683ec7e28e
Wesley Moraes <wesleyjava88@gmail.com>
2023-01-31 12:24:53 -0300

    feat: initializing the header component

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import * as Styled from './styles'
+import emojis from '../../utils/emojis'
 
 export const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length);
+        return emojis[indice]
+
+    }, [])
+
     return (<Styled.Container>
-        MainHeader
+        <h1>Toogle</h1>
+        <Styled.Profile>
+            <Styled.Welcome>
+                Olá,{emoji}
+            </Styled.Welcome>
+            <Styled.UserName>Wesley Moraes</Styled.UserName>
+        </Styled.Profile>
     </Styled.Container>)
 }
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+display: flex;
 grid-area: MH;
-color: ${props => props.theme.colors.white};
 background-color: ${props => props.theme.colors.secundary};
+justify-content: space-between;
+align-items: center;
+padding: 0 10px;
+border-bottom: 1px solid ${props => props.theme.colors.gray};
 `
+
+export const Profile = styled.div`
+color: ${props => props.theme.colors.white}
+`
+
+export const Welcome = styled.h3``
+
+export const UserName = styled.span``
