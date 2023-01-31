0f5e81e662c04f9de671d71d02d0157071c7cf8f
Wesley Moraes <wesleyjava88@gmail.com>
2023-01-30 23:31:44 -0300

    feat: provide themeGlobal in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Layout } from './components/Layout'
-
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/GlobalStyles'
+import dark from './styles/themes/dark'
+import light from './styles/themes/light'
 
 export const App: React.FC = () => {
     return (
-        <>
+        <ThemeProvider theme={dark}>
             <GlobalStyles />
             <Layout />
-        </>
+        </ThemeProvider>
     )
 }
