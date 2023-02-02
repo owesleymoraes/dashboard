431a42a119d3c490c9530c325a455ca45713ff33
Wesley Moraes <wesleyjava88@gmail.com>
2023-02-01 23:45:06 -0300

    feat: implements icon logo in aside component

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import * as Styled from './styles'
+import logoImg from '../../../public/assets/logo.svg'
+import { Icon } from '../Icon'
+
+
 
 export const Aside: React.FC = () => {
     return (
         <Styled.Container>
-            Aside
+            <Styled.Header>
+                <Icon name='logo' />
+            </Styled.Header>
+            <Styled.MenuContainer>
+                <Styled.MenuItemLink href='#'>
+                    dashboard
+                </Styled.MenuItemLink>
+            </Styled.MenuContainer>
         </Styled.Container>
     )
 
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -5,3 +5,11 @@ grid-area: AS;
 color: ${props => props.theme.colors.white};
 background-color: ${props => props.theme.colors.tertiary};
 `
+
+export const Header = styled.header``
+
+export const LogoImg = styled.img``
+
+export const MenuContainer = styled.nav``
+
+export const MenuItemLink = styled.a``
