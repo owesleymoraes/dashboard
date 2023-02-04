4c9839aa740168bab1010179e6b87efeaa7d3113
Wesley Moraes <wesleyjava88@gmail.com>
2023-02-04 17:11:30 -0300

    feat: list page preparation

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { ContentHeader } from "../../components/ContentHeader";
+import { SelectInput } from "../../components/SelectInput";
+import * as Styled from './styles'
 
 export const List: React.FC = () => {
-    return <h1>List</h1>
+    const options = [
+        { value: 'Wesley', label: 'Wesley' },
+        { value: 'Maria', label: 'Maria' },
+        { value: 'Ana', label: 'Ana' },
+    ]
+    return (
+        <Styled.ContainerListPaige>
+            <ContentHeader title="Saidas" lineColor="#E44C4E" >
+                <SelectInput options={options} />
+            </ContentHeader>
+        </Styled.ContainerListPaige>
+    )
+
 }
