4cbe87c20ae4119bc42e8fa522c066667be16d3c
Wesley Moraes <wesleyjava88@gmail.com>
2023-02-04 17:12:02 -0300

    feat: dashboard page preparation

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { ContentHeader } from "../../components/ContentHeader";
+import { SelectInput } from "../../components/SelectInput";
+import * as Styled from './styles'
 
 export const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>
+    const options = [
+        { value: 'Wesley', label: 'Wesley' },
+        { value: 'Maria', label: 'Maria' },
+        { value: 'Ana', label: 'Ana' },
+    ]
+    return (
+        <Styled.Container>
+            <ContentHeader title="Dashboard" lineColor="#F7931B" >
+                <SelectInput options={options} />
+            </ContentHeader>
+        </Styled.Container>
+    )
 }
