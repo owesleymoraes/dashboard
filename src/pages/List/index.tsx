cb83468698c734494aa0742ca69044fb6a9b26e4
Wesley Moraes <wesleyjava88@gmail.com>
2023-02-05 18:17:38 -0300

    feat: created filter button history finance

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,19 +1,103 @@
 import React from "react";
-import { ContentHeader } from "../../components/ContentHeader";
-import { SelectInput } from "../../components/SelectInput";
 import * as Styled from './styles'
+import { Content } from "../../components/Content";
+import { SelectInput } from "../../components/SelectInput";
+import { ContentHeader } from "../../components/ContentHeader";
+import { HistoryFinanceCard } from "../../components/HistoryFinanceCard";
 
 export const List: React.FC = () => {
-    const options = [
-        { value: 'Wesley', label: 'Wesley' },
-        { value: 'Maria', label: 'Maria' },
-        { value: 'Ana', label: 'Ana' },
+    const months = [
+        { value: 7, label: 'Julho' },
+        { value: 8, label: 'Agosto' },
+        { value: 9, label: 'Setembro' },
     ]
+
+    const years = [
+        { value: 2023, label: 2023 },
+        { value: 2022, label: 2022 },
+        { value: 2021, label: 2021 },
+    ]
+
     return (
         <Styled.ContainerListPaige>
-            <ContentHeader title="Saidas" lineColor="#E44C4E" >
-                <SelectInput options={options} />
+            <ContentHeader title="Saídas" lineColor="#E44C4E" >
+                <SelectInput options={months} />
+                <SelectInput options={years} />
             </ContentHeader>
+
+            <Styled.Filters>
+                <Styled.ButtonFilters colorTag={'#E44C4E'}>
+                    Recorrentes
+                </Styled.ButtonFilters>
+                <Styled.ButtonFilters colorTag={'#4E41F0'}>
+                    Eventuais
+                </Styled.ButtonFilters>
+            </Styled.Filters>
+
+            <Content>
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="05/02/2023"
+                    amount="R$ 130,00 "
+                />
+
+
+            </Content>
         </Styled.ContainerListPaige>
     )
 
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -1,3 +1,32 @@
 import styled from 'styled-components'
 
 export const ContainerListPaige = styled.div``
+
+export const Filters = styled.div`
+display: flex;
+width: 100%;
+justify-content: center;
+margin-bottom: 30px;
+
+`
+
+export const ButtonFilters = styled.button<{ colorTag: string }>`
+font-size: 18px;
+font-weight: 500;
+background: none;
+color: ${props => props.theme.colors.white};
+margin: 0 10px;
+transition: opacity .3s;
+
+&:hover{
+    opacity: .7;
+}
+
+&::after {
+    content: '';
+    display: block;
+    width: 55px;
+    margin: 0 auto;
+    border-bottom: 10px solid ${props => props.colorTag};
+}
+`
