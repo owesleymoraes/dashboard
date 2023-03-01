9f034870cad3d4f9635a28fc2deaf18a2e03053e
wesley moraes <wesleyjava88@gmail.com>
2023-02-28 21:04:32 -0300

    refactor: adjust the size of the toggle component

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,23 +1,17 @@
-import React from 'react'
-import * as Styled from './styles'
+import React from "react";
+import * as Styled from "./styles";
 
-export const Toggle = () => {
-    return (
-        <Styled.Container>
-            <Styled.ToggleLabel>Light</Styled.ToggleLabel>
-            <Styled.Label>
-                <Styled.Input type='checkbox' />
-                <Styled.Span>
-                    <Styled.Icon />
-                </Styled.Span>
-            </Styled.Label>
-            <Styled.ToggleLabel>Dark</Styled.ToggleLabel>
-        </Styled.Container>
-
-
-
-
-
-
-    )
-}
+export const Toggle: React.FC = () => {
+  return (
+    <Styled.Container>
+      <Styled.ToggleLabel>Light</Styled.ToggleLabel>
+      <Styled.Label>
+        <Styled.Input type="checkbox" />
+        <Styled.Span>
+          <Styled.Icon />
+        </Styled.Span>
+      </Styled.Label>
+      <Styled.ToggleLabel>Dark</Styled.ToggleLabel>
+    </Styled.Container>
+  );
+};
