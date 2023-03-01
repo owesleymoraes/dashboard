adbdc3b1248d608074c3d944af1d08f91067ff07
wesley moraes <wesleyjava88@gmail.com>
2023-02-28 21:42:29 -0300

    refactor: adjust the size of the contentHeader component

--- src/components/ContentHeader/index.tsx
+++ src/components/ContentHeader/index.tsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import * as Styled from './styles'
+import React from "react";
+import * as Styled from "./styles";
 
 type ContentHeaderProps = {
-    title: string,
-    lineColor: string
-    children: React.ReactNode
-}
+  title: string;
+  lineColor: string;
+  children: React.ReactNode;
+};
 
-export const ContentHeader: React.FC<ContentHeaderProps> = ({ title, lineColor, children }) => {
-
-
-
-    return (
-        <Styled.Container>
-            <Styled.TitleContainer lineColor={lineColor}>
-                {title}
-            </Styled.TitleContainer>
-            <Styled.Controllers>
-                {children}
-            </Styled.Controllers>
-        </Styled.Container>
-    )
-}
-
-
+export const ContentHeader: React.FC<ContentHeaderProps> = ({
+  title,
+  lineColor,
+  children,
+}) => {
+  return (
+    <Styled.Container>
+      <Styled.TitleContainer lineColor={lineColor}>
+        {title}
+      </Styled.TitleContainer>
+      <Styled.Controllers>{children}</Styled.Controllers>
+    </Styled.Container>
+  );
+};
--- src/components/ContentHeader/styles.ts
+++ src/components/ContentHeader/styles.ts
@@ -7,17 +7,16 @@ width: 100%;
 margin-bottom: 25px;
 `
 
-export const TitleContainer = styled.h1<{ lineColor: string }>`
+export const TitleContainer = styled.h2<{ lineColor: string }>`
 
     color: ${props => props.theme.colors.white};
 
     &::after {
         display: block;
         content: '';
-        width: 55px;
+        width: 40px;
         border-bottom: 10px solid ${props => props.lineColor};
     }
-
 `
 
 export const Controllers = styled.div`
