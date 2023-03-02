e17ff2fc93193170339e1d3703fc87a0d21c0c41
wesley moraes <wesleyjava88@gmail.com>
2023-03-02 20:35:39 -0300

    feat: getting the state in useTheme for the toggle component

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Styled from "./styles";
+import { useTheme } from "../../hooks/theme";
 
 export const Toggle: React.FC = () => {
+  const { toggleTheme} = useTheme();
+ 
   return (
     <Styled.Container>
       <Styled.ToggleLabel>Light</Styled.ToggleLabel>
       <Styled.Label>
-        <Styled.Input type="checkbox" />
+        <Styled.Input type="checkbox" onChange={toggleTheme} />
         <Styled.Span>
           <Styled.Icon />
         </Styled.Span>
