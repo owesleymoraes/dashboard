8bfb5b814afeffbce3386b1f4e47c00bb5a57a8b
wesley moraes <wesleyjava88@gmail.com>
2023-03-03 15:25:37 -0300

    feat: manipulations of colors and movements from the toggle component through the value in local storage

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import * as Styled from "./styles";
 import { useTheme } from "../../hooks/theme";
 
 export const Toggle: React.FC = () => {
-  const { toggleTheme} = useTheme();
- 
+  const { toggleTheme, theme } = useTheme();
+  const controllBackgroundToggle =
+    theme.title === "light" ? "#252A48" : "#F7933B";
+  const controllMoveToggle = theme.title === "light" ? 2 : 34;
+
   return (
     <Styled.Container>
       <Styled.ToggleLabel>Light</Styled.ToggleLabel>
       <Styled.Label>
-        <Styled.Input type="checkbox" onChange={toggleTheme} />
-        <Styled.Span>
-          <Styled.Icon />
+        <Styled.Button onClick={toggleTheme} />
+        <Styled.Span controllBackground={controllBackgroundToggle}>
+          <Styled.Icon controllMovement={controllMoveToggle} />
         </Styled.Span>
       </Styled.Label>
       <Styled.ToggleLabel>Dark</Styled.ToggleLabel>
--- src/components/Toggle/styles.ts
+++ src/components/Toggle/styles.ts
@@ -1,53 +1,47 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-align-items: center ;
+  display: flex;
+  align-items: center;
+`;
 
-`
 export const Label = styled.label`
-position: relative;
-width: 60px;
-height: 28px;
-cursor: pointer;
-margin: 0px 7px;
-`
-export const Input = styled.input`
-position: relative;
-z-index: 1;
-appearance: none;
+  position: relative;
+  width: 60px;
+  height: 28px;
+  cursor: pointer;
+  margin: 0px 7px;
+`;
 
-:checked ~ span {
-    background-color: ${props => props.theme.colors.info};
-}
+export const Button = styled.button`
+  position: relative;
+  z-index: 1;
+  appearance: none;
+`;
 
-:checked ~ span i {
-    background-color: ${props => props.theme.colors.secundary};
-    left: 32px;
-}
-`
-export const Span = styled.span`
-position: absolute;
-top: 0;
-left: 0;
-width: 100%;
-height: 100%;
-background-color: ${props => props.theme.colors.warning};
-border-radius: 80px ;
-transition: 0.5s ;
+export const Span = styled.span<{ controllBackground: string }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) => props.controllBackground};
+  border-radius: 80px;
+  transition: 0.5s;
+`;
 
-`
-export const Icon = styled.i`
-top: 2px;
-left: 4px;
-position: absolute ;
-width: 24px;
-height: 24px;
-background-color: ${props => props.theme.colors.secundary};
-border-radius: 50px;
-transition: 0.5s;
-`
+export const Icon = styled.i<{ controllMovement: number }>`
+  top: 2px;
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  background-color: ${(props) => props.theme.colors.secundary};
+  left: ${(props) => props.controllMovement + `px`};
+
+  border-radius: 50px;
+  transition: 0.5s;
+`;
 export const ToggleLabel = styled.div`
-font-size: 16px ;
-color: ${props => props.theme.colors.white} ;
-`
+  font-size: 16px;
+  color: ${(props) => props.theme.colors.white};
+`;
