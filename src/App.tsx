9d0717fd1c973f474197b2cd8cad41442099cf23
wesley moraes <wesleyjava88@gmail.com>
2023-03-03 18:51:19 -0300

    refactor: clean imports in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Routes } from './routes'
 import {useTheme} from './hooks/theme'
-import dark from './styles/themes/dark'
-import { Layout } from './components/Layout'
 import GlobalStyles from './styles/GlobalStyles'
 import { ThemeProvider } from 'styled-components'
 
