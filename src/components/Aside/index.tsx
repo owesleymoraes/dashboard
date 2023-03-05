32ada082e37356e455eabc0cf87624b6d3bcf637
wesley moraes <wesleyjava88@gmail.com>
2023-03-05 17:39:28 -0300

    feat: adding functionality of exit do aside component through useAuth

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,42 +1,40 @@
-import React from 'react'
-import { Icon } from '../Icon'
-import * as Styled from './styles'
+import React from "react";
+import { Icon } from "../Icon";
+import * as Styled from "./styles";
+import { useAuth } from "../../hooks/auth";
 import {
-    MdDashboard,
-    MdArrowDownward,
-    MdArrowUpward,
-    MdExitToApp
-} from 'react-icons/md'
-
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from "react-icons/md";
 
 export const Aside: React.FC = () => {
-    return (
-        <Styled.Container>
-            <Styled.Header>
-                <Icon name='logo' width={32} height={32} />
-                <Styled.Title>
-                    Minha carteira
-                </Styled.Title>
-            </Styled.Header>
-            <Styled.MenuContainer>
-                <Styled.MenuItemLink href='/'>
-                    <MdDashboard />
-                    Dashboard
-                </Styled.MenuItemLink>
-                <Styled.MenuItemLink href='/list/entry-balance'>
-                    <MdArrowUpward />
-                    Entradas
-                </Styled.MenuItemLink>
-                <Styled.MenuItemLink href='/list/exit-balance'>
-                    <MdArrowDownward />
-                    Saídas
-                </Styled.MenuItemLink>
-                <Styled.MenuItemLink href='#'>
-                    <MdExitToApp />
-                    Sair
-                </Styled.MenuItemLink>
-            </Styled.MenuContainer>
-        </Styled.Container>
-    )
-
-}
+  const { signOut } = useAuth();
+  return (
+    <Styled.Container>
+      <Styled.Header>
+        <Icon name="logo" width={32} height={32} />
+        <Styled.Title>Minha carteira</Styled.Title>
+      </Styled.Header>
+      <Styled.MenuContainer>
+        <Styled.MenuItemLink href="/">
+          <MdDashboard />
+          Dashboard
+        </Styled.MenuItemLink>
+        <Styled.MenuItemLink href="/list/entry-balance">
+          <MdArrowUpward />
+          Entradas
+        </Styled.MenuItemLink>
+        <Styled.MenuItemLink href="/list/exit-balance">
+          <MdArrowDownward />
+          Saídas
+        </Styled.MenuItemLink>
+        <Styled.MenuItemLButton onClick={signOut}>
+          <MdExitToApp />
+          Sair
+        </Styled.MenuItemLButton>
+      </Styled.MenuContainer>
+    </Styled.Container>
+  );
+};
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,49 +1,67 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-grid-area: AS;
-color: ${props => props.theme.colors.white};
-background-color: ${props => props.theme.colors.secundary};
-border-right: 1px solid ${props => props.theme.colors.gray};
-padding-left: 20px;
-`
+  grid-area: AS;
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.secundary};
+  border-right: 1px solid ${(props) => props.theme.colors.gray};
+  padding-left: 20px;
+`;
 
 export const Header = styled.header`
-display: flex;
-align-items: center;
-height: 70px;
-`
+  display: flex;
+  align-items: center;
+  height: 70px;
+`;
 
-export const LogoImg = styled.img``
+export const LogoImg = styled.img``;
 
 export const MenuContainer = styled.nav`
-display: flex;
-flex-direction: column;
-margin-top: 50px;
-`
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+`;
 
 export const MenuItemLink = styled.a`
-display: flex;
-align-items: center;
-font-size: 16px;
-color: ${props => props.theme.colors.info};
-text-decoration: none;
-transition: opacity .3s;
-margin: 7px 0;
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  color: ${(props) => props.theme.colors.info};
+  text-decoration: none;
+  transition: opacity 0.3s;
+  margin: 7px 0;
 
-&:hover {
-    opacity: .7;
-}
+  &:hover {
+    opacity: 0.7;
+  }
 
-> svg {
+  > svg {
     font-size: 24px;
     margin-right: 8px;
-}
+  }
+`;
+export const MenuItemLButton = styled.button`
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  border: none;
+  background: none;
+  color: ${(props) => props.theme.colors.info};
+  transition: opacity 0.3s;
+  margin: 7px 0;
 
-`
+  &:hover {
+    opacity: 0.7;
+  }
+
+  > svg {
+    font-size: 24px;
+    margin-right: 8px;
+  }
+`;
 
 export const Title = styled.h4`
-color: ${props => props.theme.colors.white};
-margin-left: 10px;
-
-`
+  color: ${(props) => props.theme.colors.white};
+  margin-left: 10px;
+`;
