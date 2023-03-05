89f8e93d413293971565359e25e88ad0b9ce0844
wesley moraes <wesleyjava88@gmail.com>
2023-03-05 17:08:53 -0300

    feat: application of a condicional of to login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+
+import { useAuth } from "../hooks/auth";
 import { SignIn } from "../pages/SignIn";
+import { AppRoutes } from "./app.routes";
 
 export const Routes: React.FC = () => {
-  return <SignIn />;
+  const { logged } = useAuth();
+  console.log(logged);
+
+  return <>{logged ? <AppRoutes /> : <SignIn />}</>;
 };
