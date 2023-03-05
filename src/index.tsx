4376b3cac20a4353a8b9635178b0ba412f8049e3
wesley moraes <wesleyjava88@gmail.com>
2023-03-05 17:07:05 -0300

    feat: provide of the usecontext of login

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
+import { AuthProvider } from "./hooks/auth";
 import { ThemeProvider } from "./hooks/theme";
 import reportWebVitals from "./reportWebVitals";
 
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
